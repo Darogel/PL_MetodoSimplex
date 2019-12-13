e46c76af326f3a148e23ace6895790f183fedb95
darogel <41120975+darogel@users.noreply.github.com>
2019-12-13 08:34:54 -0500

    Programa Inicial
    EL programa en avances, aun en proceso de desarrollo

---
+++ metodoSimplex/src/metodosimplex/MetodoSimplex.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodosimplex;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Darwin
+ */
+public class MetodoSimplex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Scanner reader = new Scanner(System.in);
+//        int intValor = 0;
+//        ArrayList<Integer> funcionObjetiva = new ArrayList<>();
+//        System.out.print("ingrese cantidad variables Función objetivo: ");
+//        int intCant = reader.nextInt();
+//        System.out.println("ingrese la Función Objetivo");
+//        for (int i = 1; i <= intCant; i++) {
+//            System.out.print("X"+i+": ");
+//            intValor = reader.nextInt();
+//            funcionObjetiva.add(intValor);
+//        }
+//        for (int i = 0; i < funcionObjetiva.size(); i++) {
+//            System.out.print("X"+i+": "+funcionObjetiva.get(i));
+//        }
+    }
+    
+}
